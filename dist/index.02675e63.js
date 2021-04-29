6da18ac1079cd27d526ace14f016c7cdd5f60f62
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-04-29 12:04:37 +0200

    Fixed the ImagePath of the Movies

--- dist/index.02675e63.js
+++ dist/index.02675e63.js
@@ -26496,7 +26496,7 @@ try {
           _id: 1,
           Title: 'The Godfather',
           Description: 'In 1945 New York City, at his daughter Connie\'s wedding to Carlo, Vito Corleone, the don of the Corleone crime family listens to requests. His youngest son, Michael, who was a Marine during World War II, introduces his girlfriend, Kay Adams, to his family at the reception.',
-          ImagePath: 'https://en.wikipedia.org/wiki/The_Godfather#/media/File:Godfather_ver1.jpg'
+          ImagePath: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/The_Godfather_movie_logo.png'
         }, {
           _id: 2,
           Title: 'Indiana Jones and the Temple od Doom',
@@ -26506,7 +26506,7 @@ try {
           _id: 3,
           Title: 'Heat',
           Description: 'A group of professional bank robbers start to feel the heat from police when they unknowingly leave a clue at their latest heist.',
-          ImagePath: 'https://en.wikipedia.org/wiki/Heat_(1995_film)#/media/File:Heatposter.jpg'
+          ImagePath: 'https://upload.wikimedia.org/wikipedia/en/6/6c/Heatposter.jpg'
         }],
         selectedMovie: null
       };
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,9 +7,9 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        {_id: 1, Title: 'The Godfather', Description: 'In 1945 New York City, at his daughter Connie\'s wedding to Carlo, Vito Corleone, the don of the Corleone crime family listens to requests. His youngest son, Michael, who was a Marine during World War II, introduces his girlfriend, Kay Adams, to his family at the reception.', ImagePath: 'https://en.wikipedia.org/wiki/The_Godfather#/media/File:Godfather_ver1.jpg'},
+        {_id: 1, Title: 'The Godfather', Description: 'In 1945 New York City, at his daughter Connie\'s wedding to Carlo, Vito Corleone, the don of the Corleone crime family listens to requests. His youngest son, Michael, who was a Marine during World War II, introduces his girlfriend, Kay Adams, to his family at the reception.', ImagePath: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/The_Godfather_movie_logo.png'},
         {_id: 2, Title: 'Indiana Jones and the Temple od Doom', Description: 'After arriving in India, Indiana Jones is asked by desperate villagers to find a mystical stone and rescue their children from a Thuggee cult practicing child slavery, black magic and ritualistic human sacrifice in honor of the goddess Kali.', ImagePath: 'https://en.wikipedia.org/wiki/Indiana_Jones_and_the_Temple_of_Doom#/media/File:Indiana_Jones_and_the_Temple_of_Doom_PosterB.jpg'},
-        {_id: 3, Title: 'Heat', Description: 'A group of professional bank robbers start to feel the heat from police when they unknowingly leave a clue at their latest heist.', ImagePath: 'https://en.wikipedia.org/wiki/Heat_(1995_film)#/media/File:Heatposter.jpg'}
+        {_id: 3, Title: 'Heat', Description: 'A group of professional bank robbers start to feel the heat from police when they unknowingly leave a clue at their latest heist.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/6/6c/Heatposter.jpg'}
       ],
       selectedMovie: null
     };
