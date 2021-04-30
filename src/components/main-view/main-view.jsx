0eff482d9a3612a1af800e6e8045d34481eccdbf
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-04-30 12:10:11 +0200

    Fetched movies from the database with componentDidMount

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 export class MainView extends React.Component {
@@ -6,13 +8,21 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {_id: 1, Title: 'The Godfather', Description: 'In 1945 New York City, at his daughter Connie\'s wedding to Carlo, Vito Corleone, the don of the Corleone crime family listens to requests. His youngest son, Michael, who was a Marine during World War II, introduces his girlfriend, Kay Adams, to his family at the reception.', ImagePath: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/The_Godfather_movie_logo.png'},
-        {_id: 2, Title: 'Indiana Jones and the Temple od Doom', Description: 'After arriving in India, Indiana Jones is asked by desperate villagers to find a mystical stone and rescue their children from a Thuggee cult practicing child slavery, black magic and ritualistic human sacrifice in honor of the goddess Kali.', ImagePath: 'https://en.wikipedia.org/wiki/Indiana_Jones_and_the_Temple_of_Doom#/media/File:Indiana_Jones_and_the_Temple_of_Doom_PosterB.jpg'},
-        {_id: 3, Title: 'Heat', Description: 'A group of professional bank robbers start to feel the heat from police when they unknowingly leave a clue at their latest heist.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/6/6c/Heatposter.jpg'}
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+  componentDidMount() {
+    axios.get('https://mytopfilms.herokuapp.com/movies')
+      .then(Response => {
+        this.setState ({
+          movies: Response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -26,17 +36,18 @@ export class MainView extends React.Component {
 
 
     if (movies.length === 0) 
-      return <div className="main-view">The list is empty!</div>;
+      return <div className="main-view" />;
     
     return (
       <div className="main-view">
         { selectedMovie 
           ? <MovieView movie={selectedMovie} onBackClick={ newSelectedMovie => { this.setSelectedMovie(newSelectedMovie);}}/>
           :movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onMovieClick={ (movie) => { this.setSelectedMovie(movie) }} />
+          <MovieCard key={movie._id} movie={movie} onMovieClick={ (newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
           ))
         }
       </div>
     );
   }
+  
 }
