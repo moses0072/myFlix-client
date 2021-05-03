f02397920199c3faf87f908a7bff7834162c6e79
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-05-03 16:27:57 +0200

    added styling with scss to the app

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,6 +7,8 @@ import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+
+import './main-view.scss';
 export class MainView extends React.Component {
   
   constructor() {
