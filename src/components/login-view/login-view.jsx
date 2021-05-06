b70fb35e9d5304887adb3ff3f1ea0ee3a85c068c
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-05-06 09:04:29 +0200

    Added new branch

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,30 +1,65 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+
 
 import './login-view.scss';
 export function LoginView(props) {
-  const [ username, setUsername] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const [usernameError, setUsernameError] = useState({});
+  const [passwordError, setPasswordError] = useState({});
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Send a request to the server for authentication
-    axios.post('https://mytopfilms.herokuapp.com/login', {
-      Username: username,
-      Password: password
-    })
-    .then(response => {
-      const data = response.data;
-      props.onLoggedIn(data);
-    })
-    .catch(e => {
-      console.log('User doesn\'t exist')
-    });  
+    console.log(username, password);
+    const isValid = formValidation();
+    if (isValid) {
+      // Send a request to the server for authentication
+      axios.post('https://mytopfilms.herokuapp.com/login', {
+        Username: username,
+        Password: password
+      })
+      .then(response => {
+       const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('User doesn\'t exist');
+      }); 
+    } 
   };
 
+   const formValidation = () => {
+     const usernameError = {};
+     const passwordError = {};
+     let isValid = true;
+   
+
+    if (username.trim().length < 5) {
+      usernameError.usernameShort = 'Username must be at least 5 characters';
+      isValid = false;
+    }
+    if (username.trim().length === 0) {
+      usernameError.userNameRequired = 'Username is required';
+      isValid = false;
+    }
+
+    if (password.trim().length < 6 ) {
+      passwordError.passwordShort= 'Password must be at least 6 characters';
+      isValid = false;
+    }
+
+    if (password.trim().length === 0) {
+      passwordError.passwordRequired = 'Password is required';
+    }
+    setUsernameError(usernameError);
+    setPasswordError(passwordError);
+    return isValid;
+  };
+  
   const toggleRegister = (e) => {
     e.preventDefault();
     props.toggleRegister();
@@ -38,11 +73,21 @@ export function LoginView(props) {
         <Form.Label>Username:</Form.Label>
         <Form.Control type='text' placeholder='Enter user name' value={username} onChange= { e => setUsername(e.target.value)} />
       </Form.Group>
+      {Object.keys(usernameError).map((key) =>{
+        return (
+          <div className='alert' key={key} >{usernameError[key]}</div>
+        );
+      })}
 
       <Form.Group controlId='formPassword'>
         <Form.Label>Password:</Form.Label>
         <Form.Control type='password' placeholder='Password' value={password} onChange= {e => setPassword(e.target.value)} />
       </Form.Group>
+      {Object.keys(passwordError).map((key) => {
+        return (
+          <div className='alert' key={key} > {passwordError[key]} </div>
+        );
+      })}
       
       <Button className='primary-btn' variant='primary' type='submit' onClick={handleSubmit}><span className='text-color'>Login</span></Button>
       <Button className='primary-btn' variant='primary' type='submit' onClick={toggleRegister}><span className='text-color'>Register</span></Button>
@@ -53,8 +98,8 @@ export function LoginView(props) {
 //Avoiding bugs with prop-types
 LoginView.propTypes ={
   user: PropTypes.shape ({
-    Username: PropTypes.string.isRequired,
-    Password: PropTypes.string.isRequired
+    Username: PropTypes.string,
+    Password: PropTypes.string
     }),
   onLoggedIn: PropTypes.func,
   toggleRegister: PropTypes.func
